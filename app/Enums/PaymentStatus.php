727bddb4c6c6d041cab25c8255c88db04e35a254
omar <omar.tamer982@gmail.com>
2026-02-22 17:02:20 +0300

    f

--- app/Enums/PaymentStatus.php
+++ app/Enums/PaymentStatus.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 enum PaymentStatus: string
 {
     case PENDING = 'pending';
-    case COMPLETED = 'success';
+    case COMPLETED = 'completed';
     case FAILED = 'failed';
     case EXPIRED = 'expired';
     case CANCELLED = 'cancelled';
