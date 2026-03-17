5401cb110e09b5b75574e2fecf71673fc9f4c917
omar <omar.tamer982@gmail.com>
2026-03-17 22:05:26 +0300

    set up welcome page

--- routes/web.php
+++ routes/web.php
@@ -12,28 +12,11 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return Inertia::render('Welcome', []);
 })->name('home');
 
 
 
 // Route::middleware(['auth', 'verified'])->group(function () {
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-
-    Route::resource('services', ServiceController::class);
-    Route::post('services/{service}/images', [ServiceController::class, 'uploadImages'])->name('services.uploadImages');
-    Route::delete('services/{service}/images/{id}', [ServiceController::class, 'deleteImage'])->name('services.deleteImage');
-    Route::resource('banner-ads', BannerAdCrudController::class);
-    Route::resource('offers', OfferCrudController::class);
-    Route::resource('orders', OrderCrudController::class);
-    Route::resource('coupons', CouponCrudController::class);
-    Route::resource('articles', ArticleCrudController::class);
-    Route::resource('companies', CompanyCrudController::class);
-});
 
 require __DIR__ . '/settings.php';
